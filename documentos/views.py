c3a044e17c1bb69e82b1307c94349ec3cfaf6f9e
avazzano <andres.vazzano@gmail.com>
2015-01-15 15:25:06 -0300

    Se agrega vista para buscar y visualizar los documentos ley. Se crea formulario para agregar/editar documento de ley.

--- documentos/views.py
+++ documentos/views.py
@@ -1,3 +1,84 @@
 from django.shortcuts import render
 
 
+from django.views import generic
+from documentos.models import ExpedienteLey
+from django.core.context_processors import request
+from documentos import forms
+from comun.models import Partido, Departamento
+
+
+
+def showExpediente(request, id):
+    
+    id= int(id)
+    
+    partidos =  Partido.objects.all()
+   
+    departamentos=Departamento.objects.all()
+    
+    if(id != 0):
+        expediente = ExpedienteLey.objects.get(numero=id)
+        return render(request, 'expediente_ley.html', {'expediente': expediente, 'partidos': partidos, 'departamentos':departamentos})
+
+   
+    else:
+        return render(request, 'expediente_ley.html', {'partidos': partidos, 'departamentos':departamentos})
+    
+   
+  
+   
+        
+
+
+def loadBusquedaExpediente(request):
+    
+        return render(request, 'expedienteley_list.html')
+    
+    
+def showResultados(request):
+    
+    expedientes= []
+    if 'id' in request.GET:
+        
+        id= int(request.GET['id'],0)
+
+        if not id:
+            id=0
+
+        if id == 0 :   
+            expedientes = ExpedienteLey.objects.all()
+        else :
+            #Se verifica que el expediente exista             
+             if(ExpedienteLey.objects.filter(numero=id).exists()):         
+                expedientes.append(  ExpedienteLey.objects.get(numero=id))
+ 
+    return render(request, 'expedienteley_list.html', {'expedientes' : expedientes})
+
+     
+    
+def saveExpediente(request):
+    
+    
+    if request.method == 'POST':
+        form = ExpedienteLeyForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            
+            send_mail(
+                cd['subject'],
+                cd['message'],
+                cd.get('email', 'noreply@example.com'),
+                ['siteowner@example.com'],
+            )
+            
+    else:
+        form = ContactForm()
+    
+    
+    return render(request, 'expedienteley_list.html')
+    
+    
+# def loadPartidos(request):
+#    partidos= Partido.objects.all()
+        
--- sstuv/urls.py
+++ sstuv/urls.py
@@ -1,17 +1,21 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-from django.views.generic.list import ListView
+from django.shortcuts import render
+from documentos import views
 
-from documentos.models import ExpedienteLey
-import documentos
+
+
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'sstuv.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
 
-    
+   
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^expedientes/$',ListView.as_view(queryset = ExpedienteLey.objects.all(),
-                                           template_name = 'expedienteley_list.html' ))    
+    
+    url(r'^expedientes/$', views.loadBusquedaExpediente),
+    url(r'^buscar/$', views.showResultados),
+    url(r'^inspeccionar/(\d+)/$', views.showExpediente),
+    url(r'^guardar/$', views.saveExpediente),
+    
+    url(r'^nuevo/(\d+)/$', views.showExpediente),
+
 )
