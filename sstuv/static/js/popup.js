78b728d8b96ba52a9cb5c115f2d3aa026980b2bb
avazzano <andres.vazzano@gmail.com>
2015-01-29 10:29:59 -0300

    Se agrega javascript.

---
+++ sstuv/static/js/popup.js
@@ -0,0 +1,38 @@
+
+
+
+$(function() {
+   
+	$( "#pase_popup" ).dialog({
+	       autoOpen: false, 
+	       height: 600,
+	       width:800 
+	    });
+	    
+	    
+	    
+	 $( "#popup" ).click(function() {
+	   	
+	       $( "#pase_popup" ).dialog( "open" );
+	    });
+	    
+	    
+	    
+	 $( "#close_popup" ).click(function() {
+	       	
+	        $( "#pase_popup" ).dialog( "close" );
+	   });
+	 
+	
+	// 
+	$("select[id^='select']").select().change(function() {
+		
+		var origen=$(this).attr('id').substring(6)
+		
+		var objetivo = "#input" + origen;
+	
+		$((objetivo)).val ( $(this).val()); 
+	});  
+    
+});
+
