70644ec326762741a4872628cb471e4f2d9822eb
avazzano <andres.vazzano@gmail.com>
2015-01-29 10:25:03 -0300

    Se agrega claves al modelo.

--- comun/models.py
+++ comun/models.py
@@ -1,13 +1,18 @@
 from django.db import models
 
 class Partido(models.Model):
+    
+    codigo= models.IntegerField(default=0)
     nombre = models.CharField(max_length=200)
-    #codigo = models.CharField(max_length=200)
+    codcas=models.CharField(max_length=20, default=0)
+
     def __str__(self):
         return self.nombre
 
 class Departamento(models.Model):
-	nombre = models.CharField(max_length=200)
-	# __unicode__ on Python 2
-	def __str__(self):
-		return self.nombre
+    
+    codigo= models.IntegerField(default=0)
+    nombre = models.CharField(max_length=200)
+    
+    def __str__(self):
+        return self.nombre
