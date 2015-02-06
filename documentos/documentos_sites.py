c6102dfd85337b1aec42e9657cc02069e49ac098
avazzano <andres.vazzano@gmail.com>
2015-02-06 10:22:06 -0300

    Se agregan url para reportes.

---
+++ documentos/documentos_sites.py
@@ -0,0 +1,29 @@
+from django.conf.urls import *
+
+from documentos.views import ExpedientesView, PasesView
+from documentos.report import PaseReport
+
+
+urlpatterns = patterns('',
+                        url(r'^expedientesLey/$', ExpedientesView.loadBusquedaExpedienteLey),
+                        url(r'^expedientes/$', ExpedientesView.loadBusquedaExpediente),
+                        url(r'^expedientes/buscar/(.*)/$', ExpedientesView.showResultados),
+                        url(r'^expediente/inspeccionar/(.*)/(\d+)/$', ExpedientesView.showExpediente),
+                        url(r'^expedientes/guardar/$', ExpedientesView.saveExpediente),
+                        url(r'^expedientes/nuevo/(.+)/(\d+)/$', ExpedientesView.showExpediente),
+                        url(r'^expedientes/editar/$', ExpedientesView.updateExpediente),
+                        url(r'^expedientes/importarExpedienteLey/$', ExpedientesView.importarExpedientesLey),
+                        url(r'^expedientes/importarExpediente/$', ExpedientesView.importarExpedientes),    
+                       
+                        url(r'^pase/guardar/$', PasesView.savePase),
+
+                            
+                        url(r'^expedientes/imprimirremito/(\d+)/(\d+)/$',PaseReport.generar ),
+
+
+#                         #Prueba    
+# #                         url(r'^pase/pdf/$', PasesView.hello_pdf),
+#                         url(r'^pase/pdff/$', PasesView.prueba_reporte),
+
+                      )
+        
