57e62a7ff36bf677b3497d3340ce42f336cf5cb8
mmanto <martin.mantovani@gmail.com>
2015-02-12 12:37:26 -0300

    se quitan archivos innecesarios se quitan archivos innecesarios

--- comun/migrations/0001_initial.py
+++ comun/migrations/0001_initial.py
@@ -13,11 +13,7 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Departamento',
             fields=[
-<<<<<<< HEAD
-                ('id', models.AutoField(auto_created=True, verbose_name='ID', primary_key=True, serialize=False)),
-=======
                 ('id', models.AutoField(verbose_name='ID', auto_created=True, primary_key=True, serialize=False)),
->>>>>>> 10ec905fd09b423b90611b4466510b330841dc11
                 ('codigo', models.IntegerField(default=0)),
                 ('nombre', models.CharField(max_length=200)),
             ],
@@ -28,17 +24,10 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Partido',
             fields=[
-<<<<<<< HEAD
-                ('id', models.AutoField(auto_created=True, verbose_name='ID', primary_key=True, serialize=False)),
-                ('codigo', models.IntegerField(default=0)),
-                ('nombre', models.CharField(max_length=200)),
-                ('codcas', models.CharField(max_length=20, default=0)),
-=======
                 ('id', models.AutoField(verbose_name='ID', auto_created=True, primary_key=True, serialize=False)),
                 ('codigo', models.IntegerField(default=0)),
                 ('nombre', models.CharField(max_length=200)),
                 ('codcas', models.CharField(default=0, max_length=20)),
->>>>>>> 10ec905fd09b423b90611b4466510b330841dc11
             ],
             options={
             },
