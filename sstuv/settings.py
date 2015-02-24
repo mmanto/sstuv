e438d0f9ddca77ee5b7c84ff6d54716f91f18deb
vldiaz <victordiaz24@gmail.com>
2015-02-24 11:45:05 -0300

    configuracion de logger

--- sstuv/settings.py
+++ sstuv/settings.py
@@ -130,7 +130,6 @@ TEMPLATE_CONTEXT_PROCESSORS = [
 # the site admins on every HTTP 500 error when DEBUG=False.
 # See http://docs.djangoproject.com/en/dev/topics/logging for
 # more details on how to customize your logging configuration.
-
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
