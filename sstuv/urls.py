2cf85442268c6acca1e180088bc0e705cc957eb9
LucasMoro <morolucas@hotmail.com>
2015-02-24 10:42:42 -0300

    merge

--- sstuv/urls.py
+++ sstuv/urls.py
@@ -25,10 +25,8 @@ urlpatterns = patterns('',
         
                    #Pase 
                                     
-                   url(r'^sig/', include(publicador_sites))  ,  
-                   
-                     
-
+                  url(r'^sig/', include(publicador_sites))  ,     
+ 
 ) + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
 
 urlpatterns += staticfiles_urlpatterns()
