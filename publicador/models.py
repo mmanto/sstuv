c3cffcb0eb32524e7ed80a666c70a16297f7c2c1
martin.mantovani <martin.mantovani@gmail.com>
2015-05-05 15:48:17 -0300

    Se corrige merge de articulos. Se quita imagen del mismo

--- publicador/models.py
+++ publicador/models.py
@@ -3,10 +3,18 @@ from distutils.command.upload import upload
 # Create your models here.
 
 class Articulo(models.Model):
-    titulo = models.CharField(max_length=200)
-    contenido = models.CharField(max_length=1500)
+    titulo = models.CharField(max_length=500)
+    contenido = models.TextField()
     fecha_creacion = models.DateTimeField(auto_now_add=True)
-    imagen = models.ImageField(upload_to='imagen_articulo')
-    
+
     def __str__(self):
         return self.titulo
+
+
+class Imagen(models.Model):
+    imagen = models.ImageField(upload_to='imagen_articulo')
+    articulo = models.ForeignKey(Articulo, related_name = 'imagenes')
+
+    def __str__(self):
+        return str(self.imagen)
+
