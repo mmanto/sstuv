cd0614793c670fbb900066af168489226a3f055a
avazzano <andres.vazzano@gmail.com>
2015-05-22 11:46:30 -0300

    Se refactoriza el paginador.

--- documentos/views.py
+++ documentos/views.py
@@ -24,6 +24,7 @@ loggerError = logging.getLogger('sstuvError')
 from django.db.models import Q 
 from django.db import connection
 from datetime import date
+from django.core import serializers
 
 
 
@@ -102,39 +103,48 @@ class ExpedientesView(ListView):
     def loadBusquedaExpedienteLey(request):
         return render(request, 'expedienteley_list.html', {'tipo' : 'ExpedienteLey'}, context_instance=RequestContext(request))
 
+    '''
+    Búsqueda de expedientes.
+    
+    '''
     @login_required(redirect_field_name='/sig/expedientes/', login_url='/sig/auth/login')
     def showResultados(request, tipo):
         
         expedientes = []
         filter_dict = {}
-
-        organismo = ExpedientesView.toInt(request.GET['organismo'])
-        numero = ExpedientesView.toInt(request.GET['numero'])
-        anio = ExpedientesView.toInt(request.GET['anio'])
-        buscarExpPropios=(request.GET['radio'])
-        filter_dict['alcance'] = alcance = ExpedientesView.toInt(request.GET['alcance'])
+        page = request.GET.get('page')
+        print (page)
         
-        if (organismo > 0):
-            filter_dict['organismo'] = organismo
-        if (numero > 0):
-            filter_dict['numero'] = numero
-        if (anio > 0):
-            filter_dict['anio'] = anio
-        if((tipo == 'ExpedienteLey')): 
-            consolidacion = bool(request.GET['consolidacion'])
-            filter_dict['consolidacion'] = consolidacion
-        if(buscarExpPropios == 'propio'):
-            filter_dict['pase_set__departamento_destino'] = request.user.groups.all().first()       
-            filter_dict['pase_set__estado']= Estado.ACEPTADO.value   
-                 
+        if( page != None  ):
+            filter_dict=request.session['filtroExpediente']
+        else:    
+           
+            # Se crea el filtro
+            organismo = ExpedientesView.toInt(request.GET['organismo'])
+            numero = ExpedientesView.toInt(request.GET['numero'])
+            anio = ExpedientesView.toInt(request.GET['anio'])
+            buscarExpPropios=(request.GET['radio'])
+            filter_dict['alcance'] = alcance = ExpedientesView.toInt(request.GET['alcance'])
+            
+            if (organismo > 0):     filter_dict['organismo'] = organismo
+            if (numero > 0):        filter_dict['numero'] = numero
+            if (anio > 0):          filter_dict['anio'] = anio
+            if((tipo == 'ExpedienteLey')): 
+                consolidacion = bool(request.GET['consolidacion'])
+                filter_dict['consolidacion'] = consolidacion
+            if(buscarExpPropios == 'propio'):
+                filter_dict['pase_set__departamento_destino__nombre'] = request.user.groups.all().first().name    
+                filter_dict['pase_set__estado']= Estado.ACEPTADO.value   
+            request.session['filtroExpediente']= filter_dict  
+                     
         if(len(filter_dict) > 0):
             if (tipo == 'Expediente'):
                 expedientes = ( Expediente.objects.filter(**filter_dict).distinct() )
             elif (tipo == 'ExpedienteLey'):
                 expedientes = ( ExpedienteLey.objects.filter(**filter_dict).distinct() )
-  
+      
         paginator = Paginator(expedientes, 10)  # Show 25 contacts per page
-        page = request.GET.get('page')
+
         try:
             expedientes = paginator.page(page)
         except PageNotAnInteger:
@@ -145,12 +155,14 @@ class ExpedientesView(ListView):
             expedientes = paginator.page(paginator.num_pages)           
         
         if (tipo == 'Expediente'):
-            return render_to_response('expedienteley_list.html', {'expedientes' : expedientes, 'tipo' : tipo, 'organismoFiltro' : organismo, 'numeroFiltro': numero, 'anioFiltro' :anio, 'alcanceFiltro': alcance, 'expPropiosFiltro':buscarExpPropios}, context_instance=RequestContext(request))
+            return render_to_response('expedienteley_list.html', {'expedientes' : expedientes, 'tipo' : tipo }, context_instance=RequestContext(request))
         else: 
-            return render_to_response('expedienteley_list.html', {'expedientes' : expedientes, 'tipo' : tipo, 'organismoFiltro' : organismo, 'numeroFiltro': numero, 'anioFiltro' :anio, 'consolidacionFiltro': request.GET['consolidacion'], 'alcanceFiltro': alcance, 'expPropiosFiltro':buscarExpPropios }, context_instance=RequestContext(request))
-
-    #PErsiste el expediente
-    # 
+            return render_to_response('expedienteley_list.html', {'expedientes' : expedientes, 'tipo' : tipo }, context_instance=RequestContext(request))
+    
+    '''    
+          Persiste el expediente
+      
+    ''' 
     @login_required(redirect_field_name='/sig/expedientes/', login_url='/sig/auth/login')
     def saveExpediente(request):
     
